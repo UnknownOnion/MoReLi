c4818c3e27095d12b15d136d9f89ac6b9daf868d
UnknownOnion <80211032+UnknownOnion@users.noreply.github.com>
2021-03-07 18:05:23 +0100

    added basic UI

---
+++ MoReLi/UC_SewingPatternDataGrid.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MoReLi
+{
+    /// <summary>
+    /// Interaktionslogik für UC_SewingPatternDataGrid.xaml
+    /// </summary>
+    public partial class UC_SewingPatternDataGrid : UserControl
+    {
+        public UC_SewingPatternDataGrid()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
